1033caf014f12bf7f214009fc455e5b0726e8728
aninha9-blip <ana.maito@escola.pr.gov.br>
2025-08-06 13:56:14 +0000

    Fizemos Cartoes no Java Script

--- app.js
+++ app.js
@@ -2,5 +2,19 @@ function criCartao(categoria, pergunta, resposta){
 const container = document.getElementById('container')
 const cartao = document.createElement('article')
 cartao.className = 'cartao'
+cartao.innerHTML = `<div class="cartao_conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="pergunta">
+                        ${pergunta}
+                    </div>
+                    <div class="resposta">
+                        ${resposta}
+                    </div>
+                </div>`
+
+
+container.appendChild(cartao)
+
+
 
 }
