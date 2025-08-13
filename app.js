fc7efb70512ce154bf29fb01322bdf202af84259
aninha9-blip <ana.maito@escola.pr.gov.br>
2025-08-13 13:49:47 +0000

    Click do botão

--- app.js
+++ app.js
@@ -2,19 +2,26 @@ function criCartao(categoria, pergunta, resposta){
 const container = document.getElementById('container')
 const cartao = document.createElement('article')
 cartao.className = 'cartao'
-cartao.innerHTML = `<div class="cartao_conteudo">
-                    <h3>${categoria}</h3>
-                    <div class="pergunta">
-                        ${pergunta}
-                    </div>
-                    <div class="resposta">
-                        ${resposta}
-                    </div>
-                </div>`
+cartao.innerHTML = `
+    <div class="cartao_conteudo">
+        <h3>${categoria}</h3>
+        <div class="pergunta">
+            ${pergunta}
+        </div>
+        <div class="resposta">
+            ${resposta}
+        </div>
+    </div>
+    `
+    
+    let respostaVisivel = false
 
+    function viraCartao(){
+        respostaVisivel = !respostaVisivel
+        cartao.classList.toggle('active', respostaVisivel)
+    }
 
-container.appendChild(cartao)
+    cartao.addEventListener('click', viraCartao)
 
-
-
+    container.appendChild(cartao)
 }
